b0ba3601d816d6d6d448b7ff95914c2611230d30
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-07-30 13:50:24 +0300

    dump area nodes only when its content differs from previous one

--- Quake/world.c
+++ Quake/world.c
@@ -1186,7 +1186,25 @@ void SV_TraceEntity(void)
 }
 
 
-static void DumpAreaNodeEdicts(FILE* f, link_t *edlink, const char* name, int level)
+#define dan_buffersize 256 * 1024
+char* dan_buffer;
+char* dan_bufferptr;
+
+static inline void dam_sprintf(const char* format, ...)
+{
+	size_t size = dan_buffersize - (dan_bufferptr - dan_buffer);
+	if (size == 0)
+		return;
+
+	va_list argptr;
+	va_start(argptr, format);
+
+	dan_bufferptr += q_vsnprintf(dan_bufferptr, size, format, argptr);
+
+	va_end(argptr);
+}
+
+static void DumpAreaNodeEdicts(link_t *edlink, const char* name, int level)
 {
 	link_t *current = edlink->next;
 	link_t *next;
@@ -1194,64 +1212,79 @@ static void DumpAreaNodeEdicts(FILE* f, link_t *edlink, const char* name, int le
 	if (current == edlink)
 		return;
 
-	fprintf(f, "%*s%s:\n", level * 2, "", name);
+	dam_sprintf("%*s%s:\n", level * 2, "", name);
 	++level;
 
 	for (/* empty */; current != edlink; current = next)
 	{
 		edict_t* ed = EDICT_FROM_AREA(current);
-		fprintf(f, "%*s- edict: %p\n", level * 2, "", ed);
+		dam_sprintf("%*s- edict: %p\n", level * 2, "", ed);
 		{
 			const char* classname = PR_GetString(ed->v.classname);
 			if (classname[0] != '\0')
-				fprintf(f, "%*s  classname: %s\n", level * 2, "", classname);
+				dam_sprintf("%*s  classname: %s\n", level * 2, "", classname);
 		}
 		{
 			const char* model = PR_GetString(ed->v.model);
 			if (model[0] != '\0')
-				fprintf(f, "%*s  model: %s\n", level * 2, "", model);
+				dam_sprintf("%*s  model: %s\n", level * 2, "", model);
 		}
 		{
 			vec_t* min = ed->v.absmin;
-			fprintf(f, "%*s  absmin: %.1f %.1f %.1f\n", level * 2, "", min[0], min[1], min[2]);
+			dam_sprintf("%*s  absmin: %.1f %.1f %.1f\n", level * 2, "", min[0], min[1], min[2]);
 		}
 		{
 			vec_t* max = ed->v.absmax;
-			fprintf(f, "%*s  absmax: %.1f %.1f %.1f\n", level * 2, "", max[0], max[1], max[2]);
+			dam_sprintf("%*s  absmax: %.1f %.1f %.1f\n", level * 2, "", max[0], max[1], max[2]);
 		}
-		fprintf(f, "%*s  solid: %i\n", level * 2, "", (int)ed->v.solid);
+		dam_sprintf("%*s  solid: %i\n", level * 2, "", (int)ed->v.solid);
 		{
 			int flags = ed->v.flags;
 			if (flags != 0)
-				fprintf(f, "%*s  flags: %i\n", level * 2, "", flags);
+				dam_sprintf("%*s  flags: %i\n", level * 2, "", flags);
 		}
 
 		next = current->next;
 	}
 }
 
-static void DumpAreaNode(FILE* f, areanode_t* areanode, int level)
+static void DumpAreaNode(areanode_t* areanode, int level)
 {
-	fprintf(f, "%*s- node: %p\n", level * 2, "", areanode);
-	fprintf(f, "%*s  axis: %i\n", level * 2, "", areanode->axis);
-	fprintf(f, "%*s  dist: %.2f\n", level * 2, "", areanode->dist);
+	dam_sprintf("%*s- node: %p\n", level * 2, "", areanode);
+	dam_sprintf("%*s  axis: %i\n", level * 2, "", areanode->axis);
+	dam_sprintf("%*s  dist: %.2f\n", level * 2, "", areanode->dist);
 
-	DumpAreaNodeEdicts(f, &areanode->solid_edicts, "solids", level + 1);
-	DumpAreaNodeEdicts(f, &areanode->trigger_edicts, "triggers", level + 1);
+	DumpAreaNodeEdicts(&areanode->solid_edicts, "solids", level + 1);
+	DumpAreaNodeEdicts(&areanode->trigger_edicts, "triggers", level + 1);
 
 	if (areanode->axis == -1)
 		return;
 
-	fprintf(f, "%*s  children:\n", level * 2, "");
-	DumpAreaNode(f, areanode->children[0], level + 2);
-	DumpAreaNode(f, areanode->children[1], level + 2);
+	dam_sprintf("%*s  children:\n", level * 2, "");
+	DumpAreaNode(areanode->children[0], level + 2);
+	DumpAreaNode(areanode->children[1], level + 2);
 }
 
 static int dan_currentframe;
 static int dan_framedumpcounter;
+static unsigned dan_hash;
 
 void SV_DumpAreaNodes(void)
 {
+	if (!dan_buffer)
+		dan_buffer = malloc(dan_buffersize);
+
+	dan_bufferptr = dan_buffer;
+
+	DumpAreaNode(sv_areanodes, 0);
+
+	unsigned hash = COM_HashString(dan_buffer);
+	if (hash == dan_hash)
+		return;
+
+	dan_hash = hash;
+
+#if 1
 	if (dan_currentframe != host_framecount)
 	{
 		dan_currentframe = host_framecount;
@@ -1260,7 +1293,6 @@ void SV_DumpAreaNodes(void)
 	else
 		++dan_framedumpcounter;
 
-#if 1
 	char nowstr[256];
 	struct tm* now = localtime(&(time_t){time(NULL)});
 	strftime(nowstr, sizeof nowstr, "%Y-%m-%d_%H-%M-%S", now);
@@ -1274,7 +1306,7 @@ void SV_DumpAreaNodes(void)
 	FILE* f = fopen(fname, "w");
 	if (f)
 	{
-		DumpAreaNode(f, sv_areanodes, 0);
+		fwrite(dan_buffer, dan_bufferptr - dan_buffer, 1, f);
 		fclose(f);
 	}
 }
