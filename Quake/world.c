4515db2c698a60cc30be3df96b1981ca39accc03
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-07-30 13:14:18 +0300

    add missing `time.h` include

--- Quake/world.c
+++ Quake/world.c
@@ -24,6 +24,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "quakedef.h"
 
+#include <time.h>
+
 /*
 
 entities never clip against themselves, or their owner
