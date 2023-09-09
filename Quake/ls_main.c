c9c4cf671d6d8d312c8c7c3050ce6f987e803a76
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-09-09 11:55:12 +0300

    adjust edict field value of entity type by one to match with the rest of lua indexing

--- Quake/ls_main.c
+++ Quake/ls_main.c
@@ -338,7 +338,7 @@ static void LS_PushEdictFieldValue(lua_State* state, etype_t type, const eval_t*
 		break;
 
 	case ev_entity:
-		lua_pushfstring(state, "entity %d", NUM_FOR_EDICT(PROG_TO_EDICT(value->edict)));
+		lua_pushfstring(state, "entity %d", NUM_FOR_EDICT(PROG_TO_EDICT(value->edict)) + 1);  // on Lua side, indices start with 1
 		break;
 
 	case ev_field:
