c77a5420ed563d998f5d040135329188d74b5a4a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2023-11-21 13:23:13 +0200

    lua: implement scrolling for list menu page
    progress #90

--- Misc/qs_pak/scripts/menus.lua
+++ Misc/qs_pak/scripts/menus.lua
@@ -68,60 +68,131 @@ local key_enter <const> = keycodes.ENTER
 local key_escape <const> = keycodes.ESCAPE
 local key_up <const> = keycodes.UPARROW
 local key_down <const> = keycodes.DOWNARROW
+local key_pageup <const> = keycodes.PGUP
+local key_pagedown <const> = keycodes.PGDN
+local key_home <const> = keycodes.HOME
+local key_end <const> = keycodes.END
+local key_kpenter <const> = keycodes.KP_ENTER
+local key_kpup <const> = keycodes.KP_UPARROW
+local key_kpdown <const> = keycodes.KP_DOWNARROW
+local key_kppageup <const> = keycodes.KP_PGUP
+local key_kppagedown <const> = keycodes.KP_PGDN
+local key_kphome <const> = keycodes.KP_HOME
+local key_kpend <const> = keycodes.KP_END
 
+local min <const> = math.min
+local max <const> = math.max
 
-local function listpage_draw(page)
-	menu.tintedtext(10, 0, page.title)
-
-	local entrycount = #page.entries
-	if entrycount == 0 then
-		return
-	end
-
-	for i = 1, entrycount do
-		menu.text(10, (i + 1) * 10, page.entries[i].text)
-	end
-
-	local cursor = page.cursor
-
-	if cursor > 0 then
-		menu.tintedtext(0, (cursor + 1) * 10, '\13')
-	end
-end
-
-local function listpage_keypress(page, keycode)
-	local cursor = page.cursor
-
-	if keycode == key_escape then
-		menu.poppage()
-		return
-	elseif keycode == key_up then
-		cursor = cursor - 1
-	elseif keycode == key_down then
-		cursor = cursor + 1
-	end
-
-	local entrycount = #page.entries
-
-	if cursor == 0 then
-		cursor = entrycount
-	elseif cursor > entrycount then
-		cursor = 1
-	end
-
-	page.cursor = cursor
-end
 
 function menu.listpage()
-	return
+	local page = 
 	{
-		title = '',
+		title = 'Title',
 		entries = {},
 		cursor = 0,
-
-		ondraw = listpage_draw,
-		onkeypress = listpage_keypress,
+		maxlines = 20,  -- for line interval of 9 pixels
+		topline = 1,
 	}
+
+	local function scrolltop()
+		page.cursor = 1
+		page.topline = 1
+	end
+
+	local function lineup()
+		if page.cursor > 1 then
+			page.cursor = page.cursor - 1
+			page.topline = page.cursor < page.topline and page.cursor or page.topline
+		else
+			page.cursor = #page.entries
+			page.topline = max(#page.entries - page.maxlines + 1, 1)
+		end
+	end
+
+	local function linedown()
+		if page.cursor < #page.entries then
+			page.cursor = page.cursor + 1
+			page.topline = page.topline + (page.cursor == page.topline + page.maxlines and 1 or 0)
+		else
+			scrolltop()
+		end
+	end
+
+	local function scrollup()
+		if page.cursor > page.maxlines then
+			page.cursor = page.cursor - page.maxlines
+			page.topline = max(page.topline - page.maxlines, 1)
+		else
+			scrolltop()
+		end
+	end
+
+	local function scrolldown()
+		local entrycount <const> = #page.entries
+		local maxlines <const> = page.maxlines
+	
+		if page.cursor + maxlines < entrycount then
+			page.cursor = page.cursor + maxlines
+			page.topline = min(page.topline + maxlines, entrycount - maxlines + 1)
+		else
+			page.cursor = entrycount
+			page.topline = max(entrycount - maxlines + 1, 1)
+		end
+	end
+
+	local function scrollend()
+		local entrycount <const> = #page.entries
+	
+		page.cursor = entrycount
+		page.topline = max(entrycount - page.maxlines + 1, 1)
+	end
+
+	page.actions =
+	{
+		[key_escape] = function() menu.poppage() end,
+		[key_up] = lineup,
+		[key_down] = linedown,
+		[key_pageup] = scrollup,
+		[key_pagedown] = scrolldown,
+		[key_home] = scrolltop,
+		[key_end] = scrollend,
+		[key_kpup] = lineup,
+		[key_kpdown] = linedown,
+		[key_kppageup] = scrollup,
+		[key_kppagedown] = scrolldown,
+		[key_kphome] = scrolltop,
+		[key_kpend] = scrollend,
+	}
+
+	page.ondraw = function (page)
+		menu.tintedtext(10, 0, page.title)
+	
+		local entrycount = #page.entries
+		if entrycount == 0 then
+			return
+		end
+	
+		local topline = page.topline
+		local cursor = page.cursor
+	
+		for i = 1, min(page.maxlines, entrycount) do
+			menu.text(10, (i + 1) * 9, page.entries[topline + i - 1].text)
+		end
+	
+		if cursor > 0 then
+			menu.tintedtext(0, (cursor - topline + 2) * 9, '\13')
+		end
+	end
+
+	page.onkeypress = function (page, keycode)
+		local action = page.actions[keycode]
+	
+		if action then
+			action()
+		end
+	end
+
+	return page
 end
 
 
