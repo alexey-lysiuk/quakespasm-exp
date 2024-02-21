bd3a4bca5d8557f5c37b2bb4b8ab76e8fd1c4eac
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-02-21 11:57:57 +0200

    move tlsf to dedicated subdirectory

--- Quake/ls_main.c
+++ Quake/ls_main.c
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "ls_common.h"
 #include "quakedef.h"
-#include "tlsf.h"
+#include "tlsf/tlsf.h"
 
 
 static lua_State* ls_state;
