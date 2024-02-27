5065da9640e5a7ba4a2c24c93c1f7adc4b7351dc
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-02-27 18:11:03 +0200

    wip [skip ci]

--- Misc/qs_pak/entfixes/generate.py
+++ Misc/qs_pak/entfixes/generate.py
@@ -5,55 +5,83 @@ import os
 import pathlib
 
 
+header = b'''/*
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+'''
+
+
 def _main():
-    self_path = pathlib.Path(__file__)
-    original_path = self_path.parent / 'original'
-    fixed_path = self_path.parent / 'fixed'
+    self_path = pathlib.Path(__file__).cwd()
+    original_path = self_path / 'original'
+    fixed_path = self_path / 'fixed'
 
-#    for filename in os.listdir(original_path):
-#        print(filename)
-#        original = open(original_path / filename, 'rb').read()
-#        fixed = open(fixed_path / filename, 'rb').read()
-#        opcodes = difflib.SequenceMatcher(a=original, b=fixed).get_opcodes()
-#        for opcode in opcodes:
-#            print(opcode)
-#        break
+    patchindex = 0
+    fixes = []
 
-    output = open('entfixes.h', 'wb')
-    output.write(b'static constexpr EF_Patch ef_patches[] =\n{\n')
+    output = open(self_path / '../../../Quake/entfixes.h', 'wb')
+    output.write(header)
+    output.write(b'\nstatic constexpr EF_Patch ef_patches[] =\n{\n')
 
-    # filename = 'e1m1@c49d.ent'
-    # filename = 'hrim_sp1@f054.ent'
-    filename = 'e2m2@fbfe.ent'
-    basename = os.path.basename(filename)
-    basenamenoext = os.path.splitext(basename)[0].encode('ascii')
-    output.write(b'\t// %s\n' % basenamenoext)
+    for filename in os.listdir(original_path):
+        print(filename)
 
-    original = open(original_path / filename, 'rb').read()
-    fixed = open(fixed_path / filename, 'rb').read()
-    opcodes = difflib.SequenceMatcher(a=original, b=fixed).get_opcodes()
-    for opcode in opcodes:
-        # print(opcode)
-        operation = opcode[0]
+        if patchindex > 0:
+            output.write(b'\n')
 
-        if operation == 'equal':
-            begin = opcode[1]
-            end = opcode[2]
-            # print(f'{{ EF_COPY, {begin}, {end - begin} }},')
-            output.write(b'\t{ EF_COPY, %u, %u },\n' % (begin, end - begin))
-            # output.write(hex(begin).encode('ascii'))
-            # , {end - begin} }},')
-        elif operation == 'insert' or operation == 'replace':
-            begin = opcode[3]
-            end = opcode[4]
-            data = fixed[begin:end]  # .decode('ascii')
-            data = data.replace(b'\n', rb'\n')
-            data = data.replace(b'"', rb'\"')
-            # print(f'{{ EF_INSERT, 0, {end - begin}, "{data}" }},')
-            output.write(b'\t{ EF_INSERT, 0, %u, "%s" },\n' % (end - begin, data))
-        # elif opcode[0] == 'replace':
+        basename = os.path.basename(filename)
+        basenamenoext = os.path.splitext(basename)[0].encode('ascii')
+        output.write(b'\t// %s\n' % basenamenoext)
 
-    output.write(b'};')
+        original = open(original_path / filename, 'rb').read()
+        fixed = open(fixed_path / filename, 'rb').read()
+        opcodes = difflib.SequenceMatcher(a=original, b=fixed).get_opcodes()
+
+        patchcount = 0
+
+        for opcode in opcodes:
+            operation = opcode[0]
+
+            if operation == 'equal':
+                begin = opcode[1]
+                end = opcode[2]
+                output.write(b'\t{ EF_COPY, %u, %u },\n' % (begin, end - begin))
+                patchcount += 1
+            elif operation == 'insert' or operation == 'replace':
+                begin = opcode[3]
+                end = opcode[4]
+                data = fixed[begin:end]
+                data = data.replace(b'\n', rb'\n')
+                data = data.replace(b'"', rb'\"')
+                output.write(b'\t{ EF_INSERT, 0, %u, "%s" },\n' % (end - begin, data))
+                patchcount += 1
+
+        mapname, crc = basenamenoext.split(b'@')
+        fixes.append((mapname, len(original) + 1, len(fixed) + 1, crc.upper(), patchindex, patchcount))
+
+        patchindex += patchcount
+
+    output.write(b'};\n\nstatic constexpr EF_Fix ef_fixes[] =\n{\n')
+
+    for fix in fixes:
+        output.write(b'\t{ "%s", %i, %i, 0x%s, %i, %i },\n' % fix)
+
+    output.write(b'};\n')
 
 
 if __name__ == '__main__':
