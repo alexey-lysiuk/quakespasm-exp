ed957153d4364fe42adafeded3bfa287fb32747f
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-04-21 09:45:52 +0300

    entfixes: update generated header with recent additions

--- Quake/entfixes.h
+++ Quake/entfixes.h
@@ -33,6 +33,10 @@ static const char* const addeddata =
 	/* 106 */ "56\"\n\"model\" \"*31\"\n}\n{\n\"speed\" \"256"
 	/* 140 */ "sm74_necros"
 	/* 151 */ "\"angle\" \"-1\"\n\"lip\" \"64"
+	/* 173 */ "20"
+	/* 175 */ "736 -2656"
+	/* 184 */ "1"
+	/* 185 */ "\"target"
 ;
 
 static constexpr EF_Patch ef_patches[] =
@@ -154,16 +158,38 @@ static constexpr EF_Patch ef_patches[] =
 	// sop1@e37d
 	{ EF_COPY, 16331, 0 },
 	{ EF_COPY, 27668, 16344 },
+
+	// casana@7cfa
+	{ EF_COPY, 6152, 0 },
+	{ EF_COPY, 272, 6227 },
+	{ EF_ADD, 2, 173 },
+	{ EF_COPY, 21344, 6501 },
+	{ EF_ADD, 9, 175 },
+	{ EF_COPY, 51, 29823 },
+	{ EF_ADD, 1, 184 },
+	{ EF_COPY, 63, 30768 },
+	{ EF_COPY, 5180, 28045 },
+	{ EF_COPY, 7381, 33286 },
+	{ EF_COPY, 11652, 40727 },
+	{ EF_COPY, 2428, 52439 },
+
+	// castlez@1699
+	{ EF_COPY, 58808, 0 },
+	{ EF_ADD, 7, 185 },
+	{ EF_COPY, 36, 58040 },
+	{ EF_COPY, 24465, 58833 },
 };
 
 static constexpr EF_Fix ef_fixes[] =
 {
+	{ "castlez", 0x1699, 83299, 83317, 98, 4 },
 	{ "coe1", 0x175a, 53514, 53501, 7, 2 },
 	{ "sm74_necros", 0x29a9, 8555, 8555, 73, 3 },
 	{ "e2m4", 0x2f38, 43875, 42917, 14, 5 },
 	{ "bbelief6", 0x324b, 64996, 64996, 0, 3 },
 	{ "sm98_zwiffle", 0x35f8, 12603, 12626, 76, 8 },
 	{ "eoem7", 0x3ea8, 55903, 56050, 19, 7 },
+	{ "casana", 0x7cfa, 54868, 54536, 86, 12 },
 	{ "dmc3m8", 0x8411, 83565, 83564, 9, 3 },
 	{ "sm27_bear", 0x92ba, 6602, 6602, 40, 3 },
 	{ "e1m4", 0x958e, 43622, 43605, 12, 2 },
