a2a0820005ad1e8cc1e5d429d2623212a3eb7010
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-05-02 13:48:17 +0300

    entfixes: generate `entfixes.h` during build
    fix #217

--- Misc/entfixes/generator/generator.cpp
+++ Misc/entfixes/generator/generator.cpp
@@ -18,10 +18,13 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include <algorithm>
-#include <cstdio>
-#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <filesystem>
 
 #include "google/vcencoder.h"
 #include "google/codetablewriter_interface.h"
@@ -158,56 +161,70 @@ public:
 
 #define EFG_VERIFY(EXPR) { if (!(EXPR)) { printf("ERROR: '%s' failed at line %i\n", #EXPR, __LINE__); exit(__LINE__); } }
 
-static std::string ReadFile(const std::string& filename, size_t& size)
+static std::pair<std::string, size_t> ReadFile(const std::filesystem::path& path)
 {
-	FILE* file = fopen(filename.c_str(), "rb");
-	EFG_VERIFY(file);
-	EFG_VERIFY(fseek(file, 0, SEEK_END) == 0);
+	std::error_code error;
+	const auto size = size_t(std::filesystem::file_size(path, error));
+	EFG_VERIFY(error.value() == 0);
+	EFG_VERIFY(size != std::uintmax_t(-1) && size != 0);
 
-	size = ftell(file);
-	EFG_VERIFY(size > 0);
-	EFG_VERIFY(fseek(file, 0, SEEK_SET) == 0);
+	std::fstream file(path, std::ios::in | std::ios::binary);
+	EFG_VERIFY(file.good());
 
 	std::string buffer;
 	buffer.resize(size);
-	EFG_VERIFY(fread(&buffer[0], 1, size, file) == size);
-	EFG_VERIFY(fclose(file) == 0);
+	file.read(&buffer[0], size);
+	EFG_VERIFY(file.good());
 
-	return buffer;
+	return { buffer, size };
 }
 
-static std::string rootpath;
-static std::string entitiespath;
-static std::vector<std::string> filenames;
+static std::filesystem::path entitiespath;
+static std::filesystem::path oldentitiespath;
+static std::filesystem::path newentitiespath;
+static std::filesystem::path outputpath;
+static std::vector<std::filesystem::path> filenames;
 
 static void GatherFileList()
 {
-	size_t filelistsize;
-	const std::string filelist = ReadFile(entitiespath + "filelist.txt", filelistsize);
-	EFG_VERIFY(!filelist.empty());
-	EFG_VERIFY(filelistsize > 0);
+	EFG_VERIFY(std::filesystem::exists(oldentitiespath));
 
-	size_t startindex = 0;
-
-	for (size_t i = 0; i < filelistsize; ++i)
-	{
-		if (filelist[i] == '\n')
-		{
-			if (i != startindex)
-				filenames.emplace_back(&filelist[startindex], i - startindex);
-
-			startindex = i + 1;
-		}
-	}
+	for (const auto& entry : std::filesystem::directory_iterator(oldentitiespath))
+		filenames.emplace_back(entry.path().filename());
 
 	EFG_VERIFY(!filenames.empty());
+
+	std::sort(filenames.begin(), filenames.end());
 }
 
-static void ProcessEntFix(const std::string& filename)
+static bool IsOutdated()
 {
-	size_t oldsize, newsize;
-	const std::string olddata = ReadFile(entitiespath + "old/" + filename, oldsize);
-	const std::string newdata = ReadFile(entitiespath + "new/" + filename, newsize);
+	if (!std::filesystem::exists(outputpath))
+		return true;
+
+	const auto headerwritetime = std::filesystem::last_write_time(outputpath);
+	const auto isoutdated = [&headerwritetime](const std::filesystem::path& path)
+	{
+		EFG_VERIFY(std::filesystem::exists(path));
+		return std::filesystem::last_write_time(path) > headerwritetime;
+	};
+
+	for (const auto& filename : filenames)
+	{
+		if (isoutdated(oldentitiespath / filename))
+			return true;
+
+		if (isoutdated(newentitiespath / filename))
+			return true;
+	}
+
+	return false;
+}
+
+static void ProcessEntFix(const std::filesystem::path& filename)
+{
+	const auto [ olddata, oldsize ] = ReadFile(oldentitiespath / filename);
+	const auto [ newdata, newsize ] = ReadFile(newentitiespath / filename);
 
 	HashedDictionary dictionary(olddata.data(), olddata.size());
 	dictionary.Init();
@@ -220,19 +237,21 @@ static void ProcessEntFix(const std::string& filename)
 	EFG_VERIFY(encoder.EncodeChunk(newdata.data(), newdata.size(), &output));
 	EFG_VERIFY(encoder.FinishEncoding(&output));
 
-	const size_t atpos = filename.find('@');
-	EFG_VERIFY(atpos != std::string::npos);
-	EFG_VERIFY(atpos < filename.size());
+	const std::string filenamestring = filename.string();
 
-	const size_t dotpos = filename.find('.');
+	const auto atpos = filenamestring.find('@');
+	EFG_VERIFY(atpos != std::string::npos);
+	EFG_VERIFY(atpos < filenamestring.size());
+
+	const auto dotpos = filenamestring.find('.');
 	EFG_VERIFY(dotpos != std::string::npos);
-	EFG_VERIFY(dotpos != filename.size());
+	EFG_VERIFY(dotpos != filenamestring.size());
 	EFG_VERIFY(dotpos > atpos);
 
-	std::string mapname{ filename.c_str(), atpos };
+	std::string mapname{ filenamestring.c_str(), atpos };
 	EFG_VERIFY(!mapname.empty());
 
-	std::string crc{ filename.c_str() + atpos + 1, dotpos - atpos - 1 };
+	std::string crc{ filenamestring.c_str() + atpos + 1, dotpos - atpos - 1 };
 	EFG_VERIFY(crc.size() == 4);  // 16-bit hex CRC
 
 	fixes.emplace_back(std::move(mapname), std::move(crc), std::move(writer->patches), oldsize + 1, newsize + 1);
@@ -290,21 +309,18 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
+static const char* const addeddata =
 )";
 
 static void WriteEntFixes()
 {
-	const std::string outputpath = rootpath + "Quake/entfixes.h";
-
-	FILE* file = fopen(outputpath.c_str(), "w");
-	EFG_VERIFY(file);
-	EFG_VERIFY(fputs(header, file) >= 0);
-	EFG_VERIFY(fputs("static const char* const addeddata =\n", file) >= 0);
+	std::ostringstream generated;
+	generated << header;
 
 	for (size_t i = 0, e = addeddata.size(); i < e; ++i)
-		EFG_VERIFY(fprintf(file, "\t/* %zu */ \"%s\"\n", addedoffsets[i], addeddata[i].c_str()) > 0);
+		generated << "\t/* " << addedoffsets[i] << " */ \"" << addeddata[i] << "\"\n";
 
-	EFG_VERIFY(fputs(";\n\nstatic constexpr EF_Patch ef_patches[] =\n{\n", file) >= 0);
+	generated << ";\n\nstatic constexpr EF_Patch ef_patches[] =\n{\n";
 
 	bool first = true;
 
@@ -313,19 +329,19 @@ static void WriteEntFixes()
 		if (first)
 			first = false;
 		else
-			EFG_VERIFY(fputs("\n", file) >= 0);
+			generated << '\n';
 
-		EFG_VERIFY(fprintf(file, "\t// %s@%s\n", fix.mapname.c_str(), fix.crc.c_str()) > 0);
+		generated << "\t// " << fix.mapname << '@' << fix.crc << '\n';
 
 		for (const EF_Patch& patch : fix.patches)
 		{
 			const EF_Operation op = patch.operation;
 			const char* opstr = op == EF_ADD ? "EF_ADD" : (op == EF_COPY ? "EF_COPY" : "EF_RUN");
-			EFG_VERIFY(fprintf(file, "\t{ %s, %zu, %zu },\n", opstr, patch.size, patch.value) > 0);
+			generated << "\t{ " << opstr << ", " << patch.size << ", " << patch.value << " },\n";
 		}
 	}
 
-	EFG_VERIFY(fputs("};\n\nstatic constexpr EF_Fix ef_fixes[] =\n{\n", file) >= 0);
+	generated << "};\n\nstatic constexpr EF_Fix ef_fixes[] =\n{\n";
 
 	std::sort(fixes.begin(), fixes.end(), [](const EF_Fix& lhs, const EF_Fix& rhs)
 	{
@@ -336,33 +352,44 @@ static void WriteEntFixes()
 
 	for (const EF_Fix& fix : fixes)
 	{
-		EFG_VERIFY(fprintf(file, "\t{ \"%s\", 0x%s, %zu, %zu, %zu, %zu },\n",
-			fix.mapname.c_str(), fix.crc.c_str(), fix.oldsize, fix.newsize, fix.patchindex, fix.patches.size()) > 0);
+		generated << "\t{ \"" << fix.mapname << "\", 0x" << fix.crc << ", "
+			<< fix.oldsize << ", " << fix.newsize << ", "
+			<< fix.patchindex << ", " << fix.patches.size() << " },\n";
 	}
 
-	EFG_VERIFY(fputs("};\n", file) >= 0);
-	EFG_VERIFY(fclose(file) == 0);
+	generated << "};\n";
+
+	std::ofstream file(outputpath, std::ios::out);
+	EFG_VERIFY(file.good());
+	file << generated.str();
+	EFG_VERIFY(file.good());
 }
 
-static void Generate(const char* rootpath)
+static void Generate(const char* entities, const char* header)
 {
-	::rootpath = rootpath;
-	::rootpath += '/';
-	entitiespath = ::rootpath + "Misc/entfixes/entities/";
+	entitiespath = entities;
+	oldentitiespath = entitiespath / "old";
+	newentitiespath = entitiespath / "new";
+
+	outputpath = header;
 
 	GatherFileList();
-	ProcessEntFixes();
-	WriteEntFixes();
+
+	if (IsOutdated())
+	{
+		ProcessEntFixes();
+		WriteEntFixes();
+	}
 }
 
 int main(int argc, const char* argv[])
 {
-	if (argc != 2)
+	if (argc != 3)
 	{
-		printf("Usage: %s repo-root-dir", argv[0]);
+		std::cout << "Usage: " << argv[0] << " entities-path generated-header-path" << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	Generate(argv[1]);
+	Generate(argv[1], argv[2]);
 	return EXIT_SUCCESS;
 }
