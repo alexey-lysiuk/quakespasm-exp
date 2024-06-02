9e7c942ec1de7d1e80c0b6a9ab70ab5388587fb7
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-06-02 13:40:22 +0300

    fix linking with msvc

--- Quake/ls_progs.cpp
+++ Quake/ls_progs.cpp
@@ -20,7 +20,11 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifdef USE_LUA_SCRIPTING
 
 #include "ls_common.h"
+
+extern "C"
+{
 #include "quakedef.h"
+}
 
 
 // Returns CRC of progdefs header (PROGHEADER_CRC)
