4ac7af22eac09ad74d1c7c208613231f603f07a1
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-07-08 16:50:06 +0300

    exp: use function to string conversion again in progs functions window
    fully qualified name is appropriate here than bare function name

--- Misc/qs_pak/scripts/expmode_progs.lua
+++ Misc/qs_pak/scripts/expmode_progs.lua
@@ -53,7 +53,7 @@ local function functions_onshow(self)
 	local entries = {}
 
 	for i, func in functions() do
-		local entry = { index = tostring(i), name = func:name() }
+		local entry = { index = tostring(i), name = tostring(func) }
 		insert(entries, entry)
 	end
 
