902067e897fd089d911309160dadb65f661c9f21
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-08-18 09:55:44 +0300

    lua: remove unused #include's

--- Quake/ls_common.h
+++ Quake/ls_common.h
@@ -21,8 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifdef USE_LUA_SCRIPTING
 
-#include <cstring>
-
 #include "lua.hpp"
 
 class LS_TempAllocatorBase
--- Quake/ls_main.cpp
+++ Quake/ls_main.cpp
@@ -19,7 +19,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifdef USE_LUA_SCRIPTING
 
-#include <algorithm>
 #include <cassert>
 
 #include "ls_common.h"
