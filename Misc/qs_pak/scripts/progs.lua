29e56ff6d163b9ddbf5ebf172323b927f182789a
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-08-23 17:28:50 +0300

    lua: add function to gather list of progs source file names
    fix #318

--- Misc/qs_pak/scripts/progs.lua
+++ Misc/qs_pak/scripts/progs.lua
@@ -1,1 +1,24 @@
 
+local insert <const> = table.insert
+local sort <const> = table.sort
+
+function progs.sourcefiles()
+	local fileset = {}
+
+	for _, func in ipairs(progs.functions) do
+		fileset[func.filename] = 0
+	end
+
+	-- Remove bogus names
+	fileset[''] = nil
+	fileset['???'] = nil
+
+	local filelist = {}
+
+	for file, _ in pairs(fileset) do
+		insert(filelist, file)
+	end
+
+	sort(filelist)
+	return filelist
+end
