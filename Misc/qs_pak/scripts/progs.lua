d4f2d9f63c1ae8def86ea9d1ca653d717a1866e9
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-08-24 13:09:45 +0300

    lua: add stub for mod detection

--- Misc/qs_pak/scripts/progs.lua
+++ Misc/qs_pak/scripts/progs.lua
@@ -22,3 +22,35 @@ function progs.sourcefiles()
 	sort(filelist)
 	return filelist
 end
+
+
+--
+-- Mod detection
+--
+
+progs.mods =
+{
+	ID1                = 0,  -- Vanilla Quake
+	HIPNOTIC           = 1,  -- Scourge of Armagon
+	ROGUE              = 2,  -- Dissolution of Eternity
+
+	-- Re-release
+	ID1_RE             = 10,
+	HIPNOTIC_RE        = 11,
+	ROGUE_RE           = 12,
+	MG1                = 13,
+
+	-- Notable mods
+	ALKALINE           = 20,
+	ARCANE_DIMENSIONS  = 21,
+	COPPER             = 22,
+	PROGS_DUMP         = 23,
+	QUOTH              = 24,
+	REMOBILIZE         = 25,
+	SPEED_MAPPING      = 26,  -- SMP
+}
+
+function progs.detectmod()
+	-- TODO: ...
+	return progs.mods.ID1
+end
