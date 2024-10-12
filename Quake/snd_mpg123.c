f914f25aa6da26cf3e52ed8e692fd00717610c32
Ozkan Sezer <sezeroz@gmail.com>
2024-10-12 23:34:28 +0300

    snd_mpg123.c: define MPG123_NO_LARGENAME
    (cherry picked from commit e576d24d5bc9d3b15d0ce7355fd779d9ca56a74e)

--- Quake/snd_mpg123.c
+++ Quake/snd_mpg123.c
@@ -25,6 +25,7 @@
 #include "snd_mp3.h"
 #include <errno.h>
 
+#define MPG123_NO_LARGENAME
 #define MPG123_DEF_SSIZE_T  /* we do define ssize_t in our stdinc.h */
 #include <mpg123.h>
 
@@ -223,4 +224,3 @@ snd_codec_t mp3_codec =
 };
 
 #endif	/* USE_CODEC_MP3 */
-
