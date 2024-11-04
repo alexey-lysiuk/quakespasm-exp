f277eb2c338683047e2f86d4a82981b33c6ab7f8
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-11-04 13:33:10 +0200

    lua: add script with new global definitions
    the sole purpose of this script is to expose custom global symbols to zed editor, so its project diagnostics won't report these globals as errors

---
+++ Misc/qs_pak/scripts/defs.lua
@@ -0,0 +1,21 @@
+ImGui = {}
+
+---@diagnostic disable: lowercase-global
+edicts = {}
+expmode = {}
+host = {}
+player = {}
+progs = {}
+sound = {}
+text = {}
+textures = {}
+vec3 = {}
+vec2 = {}
+vec4 = {}
+
+crc16 = function() end
+dprint = function() end
+expversion = function() end
+luaversion = function() end
+memstats = function() end
+stacktrace = function() end
