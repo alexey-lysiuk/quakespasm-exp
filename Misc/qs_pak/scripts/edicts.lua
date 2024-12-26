c223afa595e49138df2820e8c4d2968cde3bece7
alexey.lysiuk <alexey.lysiuk@gmail.com>
2024-12-26 10:41:23 +0200

    lua: move engine tables into own source file

--- Misc/qs_pak/scripts/edicts.lua
+++ Misc/qs_pak/scripts/edicts.lua
@@ -631,20 +631,3 @@ function edicts.boxsearch(halfedge, origin)
 
 	return result
 end
-
-
----
---- Host helpers
----
-
-function host.levelname()
-	local world = edicts[1]
-	return world and world.message
-end
-
-function host.mapname()
-	local world = edicts[1]
-
-	-- Remove leading 'maps/' and trailing '.bsp'
-	return world and world.model:sub(6):sub(1, -5)
-end
