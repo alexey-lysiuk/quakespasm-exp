bf53abc99e1494a751e6ab759217ae162abe1353
alexey.lysiuk <alexey.lysiuk@gmail.com>
2025-01-04 12:43:44 +0200

    no message

--- Misc/qs_pak/scripts/expmode_engine.lua
+++ Misc/qs_pak/scripts/expmode_engine.lua
@@ -116,10 +116,16 @@ local function levelentities_update(self)
 
 	if visible and opened then
 		local textview = self.textview or levelentities_createtextview(self)
-		local starts = self.starts
+		local savedline, savedcolumn = self.savedline, self.savedcolumn
 
+		if savedline and savedcolumn then
+			textview:SetCursorPosition(savedline, savedcolumn)
+			self.savedline, self.savedcolumn = nil, nil
+		end
+
+		local starts = self.starts
+		local currentline, currentcolumn = textview:GetCursorPosition()
 		local currententity = 0
-		local currentline = textview:GetCursorPosition()
 
 		-- Find the current entity index
 		-- TODO: Use binary search
@@ -149,6 +155,9 @@ local function levelentities_update(self)
 		end
 
 		textview:Render('##text')
+
+		self.currentline = currentline
+		self.currentcolumn = currentcolumn
 	end
 
 	imEnd()
@@ -157,6 +166,8 @@ local function levelentities_update(self)
 end
 
 local function levelentities_onhide(self)
+	self.savedline = self.currentline
+	self.savedcolumn = self.currentcolumn
 	self.textview = nil
 	self.names = nil
 	self.starts = nil
