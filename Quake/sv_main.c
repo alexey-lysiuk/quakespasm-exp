f82fe0583e78fee48dcc45d89270d30bfa610fa4
Justin <95296300+Toodles2You@users.noreply.github.com>
2026-02-12 17:39:32 -0800

    Fix svc_clientdata weapon byte potentially not being written
    (cherry picked from commit 000b15a1bcadecfa468ace47d487f6950f0cff08)

--- Quake/sv_main.c
+++ Quake/sv_main.c
@@ -878,6 +878,7 @@ void SV_WriteClientdataToMessage (edict_t *ent, sizebuf_t *msg)
 	edict_t	*other;
 	int		items;
 	eval_t	*val;
+	int		weapon;
 
 //
 // send a damage message
@@ -1015,14 +1016,16 @@ void SV_WriteClientdataToMessage (edict_t *ent, sizebuf_t *msg)
 	}
 	else
 	{
+		weapon = 0;
 		for(i=0;i<32;i++)
 		{
 			if ( ((int)ent->v.weapon) & (1<<i) )
 			{
-				MSG_WriteByte (msg, i);
+				weapon = i;
 				break;
 			}
 		}
+		MSG_WriteByte (msg, weapon);
 	}
 
 	//johnfitz -- PROTOCOL_FITZQUAKE
